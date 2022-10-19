c7ad06ab8b7879b58672ef3e9d9ce7b36322be05
Andrej R <andrej.repic04@gmail.com>
2022-10-19 14:03:23 +0200

    19-10-2022

---
+++ js/main.js
@@ -0,0 +1,28 @@
+canvas.width=width;
+canvas.height=height;
+function update()
+{
+    ctx.clearRect(0,0,width,height);
+    ctx.fillStyle = "#333";
+    ctx.beginPath();
+    if(player.screen === 15){drawMyMap1000();}
+    if(player.screen === 1){ drawMyMap();}
+    requestAnimationFrame(update);
+
+}
+
+window.addEventListener("load", function(){
+    update();
+});
+
+document.body.addEventListener("keydown", function (e)
+{
+    keys[e.keyCode] = true;
+}
+);
+
+document.body.addEventListener("keyup", function (e)
+{
+    keys[e.keyCode] = false;
+}
+);
---
+++ js/sprites.js
@@ -0,0 +1,6 @@
+var sprite=[];
+for(i=100;i<110;i++)
+{
+    sprite[i]=new Image();
+    sprite[i].src =`sprites/${i}.png`;
+}
