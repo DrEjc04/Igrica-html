de48d722d3e0a9a87ff6282a8a528ae0b1406491
Andrej R <andrej.repic04@gmail.com>
2022-10-26 14:06:00 +0200

    26.10.2022 14.05

--- js/assets.js
+++ js/assets.js
@@ -7,3 +7,20 @@ var canvas =document.querySelector("#canvas"),
     },
     press_s = {width: 500, height:40, f: 100},
     instruct = {x:126, y:300, w:900, h:57, f:103};
+    keys = [],
+    gravity = 0.3,
+    friction = 0.8;
+
+    player = {
+        x: 608,
+        y: height - 182,
+        speed: 3,
+        velX: 0,
+        velY: 0,
+        width: 35,
+        height: 52,
+        screen: 15,
+        start: 0,
+        f: 3
+        }
+        
--- js/main.js
+++ js/main.js
@@ -3,10 +3,31 @@ canvas.height=height;
 function update()
 {
     ctx.clearRect(0,0,width,height);
+    if (keys[39]) {
+        if (player.velX < player.speed) {
+        player.velX++;
+        player.f = 1;
+        }
+        }
+    if (keys[37]) {
+    if (player.velX > -player.speed) {
+        player.velX--;
+        player.f = 3
+        }
+    }
+    player.velX *= friction;
+    player.velY += gravity;
+    player.x += player.velX;
     ctx.fillStyle = "#333";
     ctx.beginPath();
     if(player.screen === 15){drawMyMap1000();}
     if(player.screen === 1){ drawMyMap();}
+    if (player.screen < 15 && player.start === 0) {
+        player.start = 1;
+        }
+    if (player.screen != 15){
+        ctx.drawImage(wiz[player.f], player.x, player.y);
+        }
     requestAnimationFrame(update);
 
 }
@@ -17,12 +38,13 @@ window.addEventListener("load", function(){
 
 document.body.addEventListener("keydown", function (e)
 {
-    keys[e] = true;
+    keys[e.keyCode] = true;
 }
 );
 
 document.body.addEventListener("keyup", function (e)
 {
-    keys[e] = false;
+    keys[e.keyCode] = false;
 }
 );
+
